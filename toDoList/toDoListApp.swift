2df7aa7886a30deb407920cea34263f8b042970f
Anika Bose <anikab1441@gmail.com>
2023-08-01 10:39:12 -0700

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Anika Bose on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
